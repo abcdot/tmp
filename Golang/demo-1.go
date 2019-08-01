8e83b7bfffc22e338ace1dfb73b00260d3e9fb07
ilearnit <helloworld.c@outlook.com>
2019-08-01 15:09:34 +0800

    Golang

---
+++ Golang/demo-1.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func main() {
+    // 单行注释
+    /* 多行 
+       or
+       块注释
+    */
+    var a string = "ILEARNIT"
+    fmt.Println(a)
+
+    var b, c int = 1, 2
+    fmt.Println(b, c)
+}
